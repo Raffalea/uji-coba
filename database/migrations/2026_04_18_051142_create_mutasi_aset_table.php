b23e586046fd39ed67c74031ca8202ce26d9dbdb
Raffalea <shaquilleraffalea@gmail.com>
2026-04-18 12:28:37 +0700

    migration

---
+++ database/migrations/2026_04_18_051142_create_mutasi_aset_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mutasi_aset', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('aset_id')->constrained('aset');
+            $table->foreignId('lokasi_asal_id')->constrained('lokasi');
+            $table->foreignId('lokasi_tujuan_id')->constrained('lokasi');
+            $table->date('tanggal_mutasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mutasi_asets');
+    }
+};
