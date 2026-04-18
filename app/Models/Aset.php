3e80eb57d37c4921d2b902ec1de3f6c48fa215a3
Raffalea <shaquilleraffalea@gmail.com>
2026-04-18 12:37:10 +0700

    model relation

--- app/Models/Aset.php
+++ app/Models/Aset.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Aset extends Model
 {
     //
+    public function kategori()
+    {
+        return $this->belongsTo(KategoriAset::class, 'kategori_id');
+    }
+
+    public function lokasi()
+    {
+        return $this->belongsTo(Lokasi::class, 'lokasi_id');
+    }
 }
--- app/Models/MutasiAset.php
+++ app/Models/MutasiAset.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class MutasiAset extends Model
 {
     //
+    public function aset()
+    {
+        return $this->belongsTo(Aset::class, 'aset_id');
+    }
+
+    public function lokasiAsal()
+    {
+        return $this->belongsTo(Aset::class, 'lokasi_asal_id');
+    }
+
+    public function lokasiTujuan()
+    {
+        return $this->belongsTo(Aset::class, 'lokasi_tujuan_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
